09e96758a4c7f43376f87e81d96dbfed564e16d3
Rulebreakr <96858635+Rulebreakr@users.noreply.github.com>
2022-10-06 16:43:03 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,46 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "10aaada5",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Enter radius of circle : 1.1\n",
+      "Area of the circle is: 3.8013271108436504\n"
+     ]
+    }
+   ],
+   "source": [
+    "from math import pi\n",
+    "r = float(input (\"Enter radius of circle : \"))\n",
+    "print (\"Area of the circle is: \" + str(pi * r**2))"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
